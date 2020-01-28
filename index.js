5e49a31efbe3078398b75aa6d3786eced0300b1a
itxDevCory <itxDevCory@gmail.com>
2020-01-28 18:45:44 -0500

    initial commit for developer profile generator

---
+++ index.js
@@ -0,0 +1,23 @@
+const questions = [function(2)
+
+];
+console.log(window)
+if
+
+function writeToFile(fileName, data) {
+
+}
+
+function init() {
+
+    init();
+    var http = require("http");
+    var PORT = 8080;
+
+    function handleRequest(request, response) {
+        response.end("It Works!! Path Hit: " + request.url);
+    }
+    var server = http.createServer(handleRequest);
+    server.listen(PORT, function() {
+        console.log("Server listening on: http://localhost:" + PORT);
+    });
